53ef62e3012ee07a80c1acf904f0467b874b92c3
Fernando Sandoval <47694239+FerchoSG@users.noreply.github.com>
2021-05-22 03:10:10 -0600

    formatting date

--- src/components/CustomDatePicker.jsx
+++ src/components/CustomDatePicker.jsx
@@ -9,6 +9,7 @@ export default function CustomDatePicker({selectedDate, handleDateChange}) {
     const handleChange = (value) => {
         let date = new Date(value).toLocaleDateString()
         let dateArray = date.split('/')
+        dateArray[0] < 10 ? dateArray[0] = `0${dateArray[0]}`:  null;
         let formattedDate = `${dateArray[2]}-${dateArray[0]}-${dateArray[1]}`
         setTest(formattedDate)
         handleDateChange(formattedDate)
