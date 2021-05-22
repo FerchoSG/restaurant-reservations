36a2b381f362bbe7aa70c76de7197bfbc0aa4e4a
Fernando Sandoval <47694239+FerchoSG@users.noreply.github.com>
2021-05-22 04:26:11 -0600

    formatting date

--- src/hooks/useDate.jsx
+++ src/hooks/useDate.jsx
@@ -5,9 +5,9 @@ export default function useDate(){
     const [selectedDate, updateDate] = useState()
 
     useEffect(()=>{
-        let sixHours = 60 * 60 * 6 * 1000;
-        let currentTime = new Date() - sixHours
-        let date = new Date(currentTime).toJSON()
+       // let sixHours = 60 * 60 * 6 * 1000;
+        //let currentTime = new Date() - sixHours
+        let date = new Date().toJSON()
         let formattedDate = date.split('T')[0]
         updateDate(formattedDate)
     },[])
