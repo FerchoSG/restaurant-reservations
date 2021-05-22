16c679b207ef21a55c1d9c1c7f634347d8391355
Fernando Sandoval <47694239+FerchoSG@users.noreply.github.com>
2021-05-22 03:58:50 -0600

    formatting date

--- src/hooks/useDate.jsx
+++ src/hooks/useDate.jsx
@@ -7,7 +7,7 @@ export default function useDate(){
     useEffect(()=>{
         let date = new Date()
         let month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`
-        let day = date.getDate() + 1 > 9 ? date.getDate() + 1 : `0${date.getDate() + 1}`
+        let day = date.getDate() > 9 ? date.getDate() : `0${date.getDate() }`
         let formattedDate = `${date.getFullYear()}-${month}-${day}`
         updateDate(formattedDate)
     },[])
