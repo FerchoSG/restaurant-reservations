d90a5a20c6a36c311f576921cfd400b036f11d48
Fernando Sandoval <sandovalf193@gmail.com>
2021-05-25 19:41:36 -0600

    linked to home added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useAuth} from '../context/AuthContext'
 import logo from '../logo.png'
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
 
@@ -8,9 +9,11 @@ export default function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-ligh d-flex align-items-center justify-content-between p-2">
             <div className="d-flex align-items-center gap-2">
-                <img 
-                    src={logo}
-                    alt="avatar"  style={{width: '60px', objectFit: 'fill'}} />
+                <Link to="/">
+                    <img 
+                        src={logo}
+                        alt="avatar"  style={{width: '60px', objectFit: 'fill'}} />
+                </Link>
                 <h3 className="nav-title text-center">Arenal Springs Resort & Spa</h3>
             </div>
             <div>
